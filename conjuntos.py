a17ed2f93bc7d70e647cfea074eec0f81bbbc658
Zyanya Mireles <164534002+Zyanya-Mireles-1080@users.noreply.github.com>
2024-09-06 11:59:44 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,6 @@
+misamigos = {"Iván","Yareli","Andrea"}
+print(misamigos)
+
+print("Uso de for en conjuntos")
+for susnombres in misamigos:
+    print(susnombres)
---
+++ diccionario.py
@@ -0,0 +1,11 @@
+misdatos = {
+    "Matrícula":1080,
+    "Nombre": "Zyanya",
+    "Edad": 17,
+    "Especialidad": "Programacion"
+}
+print(misdatos)
+
+print("Uso de for en diccionarios")
+for Dingresar in misdatos:
+    print(Dingresar)
